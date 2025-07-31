e3e95a7a58b7c9ef365af0e3805a5f4792915880
CharlesPatterson <pattercm@gmail.com>
2025-07-31 14:39:58 -0700

    Remove html duplication in footer and header components

--- src/app/ui/components/footer.tsx
+++ src/app/ui/components/footer.tsx
@@ -1,38 +1,43 @@
 import Image from "next/image";
 
+const social_links = [
+  {
+    name: "Github",
+    destination: "https://github.com/ua-cpatterson",
+    svg: "/github-octocat-svgrepo-com.svg",
+  },
+  {
+    name: "LinkedIn",
+    destination:
+      "https://www.linkedin.com/in/charles-patterson-fullstack-engineer/",
+    svg: "/linkedin-color-svgrepo-com.svg",
+  },
+];
+
 export default function Footer() {
   return (
     <footer className="font-mono row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      <a
-        href="https://github.com/ua-cpatterson"
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/github-octocat-svgrepo-com.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        Github
-      </a>
-      <a
-        href="https://www.linkedin.com/in/charles-patterson-fullstack-engineer/"
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/linkedin-color-svgrepo-com.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        LinkedIn
-      </a>
+      {social_links.map(({ name, destination, svg }) => {
+        return (
+          <a
+            key={name}
+            href={destination}
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src={svg}
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            {name}
+          </a>
+        );
+      })}
+
       <p>&copy; {new Date().getFullYear()} Charles Patterson</p>
     </footer>
   );
--- src/app/ui/components/header.tsx
+++ src/app/ui/components/header.tsx
@@ -1,28 +1,38 @@
 import Link from "next/link";
 
+const links = [
+  {
+    name: "Home",
+    destination: "/",
+  },
+  {
+    name: "About",
+    destination: "about",
+  },
+  {
+    name: "Contact",
+    destination: "contact",
+  },
+  {
+    name: "Resume",
+    destination: "resume",
+  },
+];
+
 export default function Header() {
   return (
-    <>
-      <div className="font-mono row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <Link
-          href="/"
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          Home
-        </Link>
-        <Link
-          href="about"
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          About
-        </Link>
-        <Link
-          href="contact"
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          Contact
-        </Link>
-      </div>
-    </>
+    <div className="font-mono row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {links.map(({ name, destination }) => {
+        return (
+          <Link
+            key={name}
+            href={destination}
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+          >
+            {name}
+          </Link>
+        );
+      })}
+    </div>
   );
 }
