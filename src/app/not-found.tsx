e03f13324d6fb9c2572a26893caa27005052103f
CharlesPatterson <pattercm@gmail.com>
2025-07-31 15:08:35 -0700

    Add a Resume page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <Head>
         <title>Charles Patterson&apos;s Personal Site - 404</title>
       </Head>
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+export default function Resume() {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div className="grid items-center justify-items-center pt-5">
+      <Document
+        file="Resume-CharlesPatterson.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p className="text-center">
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
