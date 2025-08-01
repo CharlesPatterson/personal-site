0e16448cb8642132b47f7cf9b9862fdaf71bc4cd
CharlesPatterson <pattercm@gmail.com>
2025-08-01 14:35:03 -0700

    Add copy to About page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,32 +11,69 @@ export default function Home() {
           width='300'
           height='100'
         ></Image>
-        <p>
-          I'm a software engineer with over 10+ years of experience as an
-          engineer and 3+ years experience leading development teams.
-        </p>
-        <p>
-          Technologies I enjoy working with:
-          <ol>
-            <li>Next.js</li>
-            <li>Node.js</li>
-            <li>Typescript/Javascript</li>
-            <li>GraphQL</li>
-            <li>Python</li>
-            <li>AWS</li>
-            <li>Postgres</li>
+        <div>
+          <p>
+            I&apos;m a software engineer with over 10+ years of industry
+            experience and 3+ years experience leading development teams.
+          </p>
+          <p>
+            My background is as a generalist that has worked across the full
+            stack: from front-end to back-end to builds and infrastructure.
+          </p>
+          <br />
+          <p>
+            I currently live in Bellevue, Washington with my girlfriend and our
+            three cats.
+          </p>
+          <br />
+          <span className='font-bold'>
+            Companies and Industries I&apos;ve worked in:
+          </span>
+          <ol className='list-inside list-disc'>
+            <li>Under Armour - E-Commerce</li>
+            <li>Springbox - Digital Marketing</li>
+            <li>Compass Learning/Edgenuity - EdTech</li>
+            <li>E2open - Supply Chain Management</li>
           </ol>
           <br />
-          Tools I Enjoy:
-          <ol>
-            <li>VS Code</li>
-            <li>VIM</li>
-            <li>Postman</li>
-            <li>zsh</li>
-            <li></li>
-            <li></li>
+          <span className='font-bold'>
+            Technologies I&apos;ve enjoyed working with:
+          </span>
+          <ol className='list-inside list-disc'>
+            <li>Languages: Typescript | Javascript | Python</li>
+            <li>
+              Frameworks/Libraries: Next.js | Express.js | FastAPI | Tailwind |
+              React
+            </li>
+            <li>Builds: Github Actions | Jenkins</li>
+            <li>APIs: GraphQL | REST</li>
+            <li>Infrastructure / Cloud: AWS, Kubernetes, Docker, Terraform</li>
+            <li>Databases/Storage: Postgres | DynamoDB | Redis</li>
+            <li>APM: Datadog</li>
           </ol>
-        </p>
+          <br />
+          <span className='font-bold'>Tools I Frequently Use:</span>
+          <ol className='list-inside list-disc'>
+            <li>Editors: VS Code | Textmate | VIM</li>
+            <li>Devtools: Git | Postman | Chrome DevTools | zsh</li>
+            <li>
+              Documentation: Swagger | Lucidchart | Excalidraw | Confluence |
+              JIRA
+            </li>
+            <li>Communication: Slack, Outlook</li>
+            <li>Microsoft Excel: Tables | Macros | XLookups</li>
+          </ol>
+          <br />
+          <span className='font-bold'>Personal Interests</span>
+          <ol className='list-inside list-disc'>
+            <li>Rock Climbing</li>
+            <li>DIY / Home Improvement</li>
+            <li>Improv Comedy</li>
+            <li>Cats</li>
+            <li>Chess</li>
+            <li>Videogames</li>
+          </ol>
+        </div>
       </main>
     </div>
   );
