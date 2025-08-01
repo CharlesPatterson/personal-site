ea6961637e6de70d9a11e8f4692dc330e81bd5d9
CharlesPatterson <pattercm@gmail.com>
2025-08-01 14:58:36 -0700

    Update 404 page to fit page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,12 +2,11 @@ import Head from 'next/head';
 
 export default function NotFound() {
   return (
-    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
       <Head>
         <title>Charles Patterson&apos;s Personal Site - 404</title>
       </Head>
-      <h2 className=''>Not Found</h2>
-      <p>Congratulations, you found my 404 not-found page!</p>
+      <h2 className='text-2xl'>Page Not Found</h2>
     </div>
   );
 }
