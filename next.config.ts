74376f95486c3b72a82166706f0715db6bc039dd
CharlesPatterson <pattercm@gmail.com>
2025-08-01 20:05:27 -0700

    Add Github Actions workflows

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/personal-site',
+  output: 'export',
+  reactStrictMode: true,
   /* config options here */
 };
 
