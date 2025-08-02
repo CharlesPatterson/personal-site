cf71c1d125f722679eb560ada0b488a19c0feda6
CharlesPatterson <pattercm@gmail.com>
2025-08-01 20:28:12 -0700

    Remove unused api endpoint

--- src/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest } from "next";
-
-function handler(req: NextApiRequest) {
-  return Response.json({ message: "Hello, Next.js API!" });
-}
-
-export { handler as GET, handler as POST };
