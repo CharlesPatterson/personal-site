392714e8cd92b092b233bd8030989ceef8ea8f63
CharlesPatterson <pattercm@gmail.com>
2025-08-10 17:03:38 -0700

    Remove vercel.svg

--- src/lib/ui/components/Footer.tsx
+++ src/lib/ui/components/Footer.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import NextJSLogo from '~icons/logos/nextjs-icon.jsx';
 
 export default function Footer() {
   return (
@@ -10,14 +10,8 @@ export default function Footer() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          <Image
-            aria-hidden
-            src='/personal-site/vercel.svg'
-            alt='File icon'
-            width={16}
-            height={16}
-          />
           Powered by Next.js
+          <NextJSLogo className='inline-block' />
         </a>
       </p>
       <p>&copy; {new Date().getFullYear()} Charles Patterson</p>
