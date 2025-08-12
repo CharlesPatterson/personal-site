ba3a1e759333f3d813caf3539ee5bd38b2c6c2c9
CharlesPatterson <pattercm@gmail.com>
2025-08-12 12:06:58 -0700

    Remove changing base/asset paths

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 import unpluginIcons from 'unplugin-icons/webpack';
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NODE_ENV === 'development' ? '' : '/personal-site',
-  assetPrefix: process.env.NODE_ENV === 'development' ? '/' : '/personal-site/',
   output: 'export',
   reactStrictMode: true,
   images: {
@@ -19,7 +17,6 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  /* config options here */
 };
 
 export default nextConfig;
